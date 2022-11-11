1fa0896a1ebe85a2e72b520f79fc4cf97f5dd82a
sometimesbread <82068136+sometimesbread@users.noreply.github.com>
2022-11-10 19:28:23 -0800

    added a bunch of classes
    Yep

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var calculator = Desmos.GraphingCalculator(elt);
 //other vars lol
 const maxTriangles = 64; //increasing this will slow the calculator down (a lot)
 
+Initialize();
+calculator.setExpression({id: '0', latex: GetTriangleLatex(5, 3, 2, 3, 5, 10), color: '#FF0000'});
+
 //returns latex of a triangle
 function GetTriangleLatex(x1, y1, x2, y2, x3, y3)
 {
@@ -16,10 +19,40 @@ function Initialize()
 {
     for(let i = 0; i < maxTriangles; i++)
     {
-        calculator.setExpression({id: i.toString(), latex: '(-10, 0)'});
-        console.log("ASdasf")
+        calculator.setExpression({id: i.toString(), latex: '(-10, 0)'});    
     }
 }
 
-Initialize();
-calculator.setExpression({id: '0', latex: GetTriangleLatex(5, 3, 2, 3, 5, 10), color: '#FF0000'});
+class mat4x4
+{
+    constructor()
+    {
+        this.m = [ [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0] ];
+    }
+}
+
+class vector3
+{
+    constructor()
+    {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+    }
+}
+
+class triangle
+{
+    constructor()
+    {
+        this.p = [new vector3(), new vector3(), new vector3()]
+    }
+}
+
+class mesh 
+{
+    constructor()
+    {
+        this.tris = []
+    }
+}
